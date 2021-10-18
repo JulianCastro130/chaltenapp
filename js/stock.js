5c532b61045262571871d270060f636e3e24f901
Julian Castro <cococastro619@gmail.com>
2021-10-18 18:42:58 -0300

    Hotel

---
+++ js/stock.js
@@ -0,0 +1,11 @@
+let stockProductos = [
+    {id: 1, nombre: "Hotel 1", tipo: "hotel", desc: "Habitación doble", precio: 4000, desayuno: "Si", img: 'https://picsum.photos/200/300'},
+    {id: 2, nombre: "Hotel 2", tipo: "hotel", desc: "Habitación doble", precio: 4500, desayuno: "Si", img: 'https://picsum.photos/200/300'},
+    {id: 3, nombre: "Hotel 3", tipo: "hotel", desc: "Habitación doble", precio: 5000, desayuno: "Si", img: 'https://picsum.photos/200/300'},
+    {id: 4, nombre: "Hotel 4", tipo: "hotel", desc: "Habitación doble", precio: 5500, desayuno: "Si", img: 'https://picsum.photos/200/300'},
+    {id: 5, nombre: "Hostel 1", tipo: "hostel", desc: "Habitación compartida", precio: 1800, desayuno: "No", img: 'https://picsum.photos/200/300'},
+    {id: 6, nombre: "Hostel 2", tipo: "hostel", desc: "Habitación compartida", precio: 2000, desayuno: "No", img: 'https://picsum.photos/200/300'},
+    {id: 7, nombre: "Hostel 3", tipo: "hostel", desc: "Habitación compartida", precio: 2000, desayuno: "No", img: 'https://picsum.photos/200/300'},
+    {id: 8, nombre: "Cabaña 1", tipo: "cabaña", desc: "Cabaña cuatro personas", precio: 6000, desayuno: "Si", img: 'https://picsum.photos/200/300'},
+    {id: 9, nombre: "Cabaña 2", tipo: "cabaña", desc: "Cabaña cinco personas", precio: 7000, desayuno: "No", img: 'https://picsum.photos/200/300'},
+]
